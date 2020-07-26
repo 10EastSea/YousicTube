48928eeb015beb49fcc040e0a14a6e2735bdba91
10EastSea <xgf6580@gmail.com>
2020-07-26 20:59:45 +0900

    domain add_2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['101.101.216.109']
+ALLOWED_HOSTS = ['101.101.216.109', 'yousictube.ml', 'www.yousictube.ml']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
